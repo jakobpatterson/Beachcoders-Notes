bd441b67796426061355d69f113a45854a30d92b
Jakob Patterson <jakobpatterson@JakobPaMacBook4.home>
2022-09-12 19:03:48 -0700

    notes from day2-4

---
+++ DayThree/app.js
@@ -0,0 +1,66 @@
+
+// console.log("Hello");
+// console.error("Error");
+// console.log(3+4, "hello", 2*2);
+// let num = 17
+// console.log("num", num);
+// if (1 === 1){
+//     console.log("hello");
+// }
+// if (1 <= 1) {
+//     console.log("true");
+
+// }
+// const bankBalance = 2;
+// if (bankBalance === 50) {
+//     console.log("Please Insert More Money");
+    
+// } else if(bankBalance === 100){
+//     console.log("Congrats!!!");
+
+// } else if(bankBalance === 0) {
+//     console.log("You're Broke");
+
+// }
+// else {
+//     console.log("You Have Money");
+// }
+// let highScore = 2500;
+// let playerScore = 400;
+// if(playerScore >= highScore) {
+//     console.log("Congrats New High Score!!!!!!!!!");
+//     highScore = playerScore;
+// } else {
+//     console.log("You Lose");
+// }
+// let password = "super dude";
+// if (password.length >= 6) {
+//     if (password.indexOf(" ") !== -1) {
+//         console.log("no spaces allowed in password");
+//     } else {
+//         console.log("Password Accepted");
+//     }
+// } else {
+//     console.log("Password needs to be longer");
+// }
+// let mysteryNum;
+// if (mysteryNum) {
+//     console.log("truthy");
+
+// }else {
+//     console.log("falsy");
+// }
+// let mysteryNum = " ";
+// if (mysteryNum) {
+//     console.log("truthy");
+// }else {
+//     console.log("falsy");
+// }
+
+// 1===1 && 1!==0 
+let password = "super dude" 
+if (password.length >= 6 && password.indexOf(" ") === -1) {
+    console.log("Password Accepted");
+} else {
+    console.error("invalid");
+}
